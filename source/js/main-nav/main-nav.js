2907c9c9c4d79195137a72a3ab546b61914c0db2
IC1101 <stas-s2000@mail.ru>
2020-03-07 21:42:30 +0300

    Отрефакторил код

--- source/js/main-nav/main-nav.js
+++ source/js/main-nav/main-nav.js
@@ -4,11 +4,6 @@ var mainNavToggle = document.querySelector(".main-nav__toggle");
 mainNav.classList.remove("main-nav--nojs");
 
 mainNavToggle.addEventListener("click", function () {
-	if (mainNav.classList.contains("main-nav--closed")) {
-		mainNav.classList.remove("main-nav--closed");
-		mainNav.classList.add("main-nav--opened");
-	} else {
-		mainNav.classList.remove("main-nav--opened");
-		mainNav.classList.add("main-nav--closed"); 
-	}
+	mainNav.classList.toggle("main-nav--closed");
+	mainNav.classList.toggle("main-nav--opened");
 });
