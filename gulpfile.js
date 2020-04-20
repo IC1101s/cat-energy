f01942cc6739f4d78dfb2111d30f5087810f681b
IC1101 <stas-s2000@mail.ru>
2020-04-20 15:42:03 +0300

    Дополнил комментариями и сделал правки

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var webp = require("gulp-webp");
 var del = require("del");
 var server = require("browser-sync").create();
 
+/* gulp html - копирует html файлы в папку build и инклюдит (добавляет) спрайт в html */
 gulp.task("html", function () {
 	return gulp.src("source/*.html")
 	.pipe(posthtml([
@@ -24,6 +25,11 @@ gulp.task("html", function () {
 	.pipe(gulp.dest("build/"));
 });
 
+/* gulp css - plumber проверяет на ошибки, sourcemap показывает исходное 
+состаяние scss(less) в DevTools (и т.п.), sass преобразует css в sass(scss), postcss включает autoprefixer,
+происходит добавление файла style.css в папку build, csso минифицирует style.css,
+rename переименовывает в style.min.css, заканчивается слежка за файлами через sourcemap,
+файл style.min.css и его map добавляется в папку build, и происходит перезагрузка страницы */
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
@@ -40,6 +46,8 @@ gulp.task("css", function () {
     .pipe(server.stream());
 });
 
+/* gulp sprite - берет файлы которые нужно преобразовать в symbol и с помощью
+svgstore преобразует их */
 gulp.task("sprite", function () {
 	return gulp.src("source/img/**/{icon-social-*,icon-contacts-*,htmlacademy*}.svg")
 	.pipe(svgstore({
@@ -49,6 +57,8 @@ gulp.task("sprite", function () {
 	.pipe(gulp.dest("build/img"));
 });
 
+/* gulp images - optipng оптимизирует png, mozjpeg оптимизирует jpeg, 
+svgo оптимизирует svg (убирает лишнее) */
 gulp.task("images", function () {
 	return gulp.src("source/img/**/*.{png,jpg,svg}")
 	.pipe(imagemin([
@@ -59,16 +69,19 @@ gulp.task("images", function () {
 	.pipe(gulp.dest("source/img"));
 });
 
+/* gulp webp - преобразует png и jpeg в webp (90% качества) */
 gulp.task("webp", function () {
 	return gulp.src("source/img/**/*.{png,jpg}")
 	.pipe(webp({quality: 90}))
 	.pipe(gulp.dest("source/img"));
 });
 
+/* gulp clean - удаляет всё из папки build */
 gulp.task("clean", function () {
 	return del("build");
 });
 
+/* gulp copy - копирует fonts, img, js, ico в папку build */
 gulp.task("copy", function () {
 	return gulp.src([
 		"source/fonts/**/*.{woff,woff2}",
@@ -81,6 +94,7 @@ gulp.task("copy", function () {
 	.pipe(gulp.dest("build/"));
 });
 
+/* gulp server - запускает сервер через папку build */
 gulp.task("server", function () {
   server.init({
     server: "build/",
@@ -90,17 +104,19 @@ gulp.task("server", function () {
     ui: false
   });
 
+  /* слежка за файлами которые надо перезапустить */
   gulp.watch("source/sass/**/*.scss", gulp.series("css"));
-  gulp.watch("source/images/**", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/js/**/*.js", gulp.series("build", "refresh"));
 });
 
+/* gulp refresh - перезапускает сервер */
 gulp.task("refresh", function (done) {
 	server.reload();
 	done();
 });
 
+/* gulp build - запускает команды, которые перечислены ниже */
 gulp.task("build", gulp.series(
 	"clean", 
 	"copy",   
@@ -109,4 +125,5 @@ gulp.task("build", gulp.series(
 	"html"
 ));
 
+/* gulp start - запускает команды, которые перечислены в папке build и запускает сервер */
 gulp.task("start", gulp.series("build", "server"));
